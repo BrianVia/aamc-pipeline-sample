fe4e405523944255910476e1df309eb58056dd35
Brian Via <brian.via.dev@gmail.com>
2020-06-10 07:34:12 -0400

    Remove test changes

--- apps/products/src/app/app.component.ts
+++ apps/products/src/app/app.component.ts
@@ -7,6 +7,4 @@ import '@nx-example/shared/header';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  // this is a comment
-}
+export class AppComponent {}
